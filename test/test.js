5f37b7cd3d35a45bb1f8fa80f17177fdc5738981
Pisey Tep <piseytep26@gmail.com>
2025-10-31 14:49:18 +0700

    Update greeting message

--- test/test.js
+++ test/test.js
@@ -2,9 +2,11 @@ const request = require('supertest');
 const app = require('../app');
 const { expect } = require('chai');
 describe('GET /', () => {
-it('should return Hello, GitHub Actions!', async () => {
-const res = await request(app).get('/');
+it('should return Hello, CI/CD!', async () => {
+// it('should return Hello, GitHub Actions!', async () => { --- IGNORE ---
+    const res = await request(app).get('/');
 expect(res.status).to.equal(200);
-expect(res.text).to.equal('Hello, GitHub Actions!');
+expect(res.text).to.equal('Hello, CI/CD!');
+// expect(res.text).to.equal('Hello, GitHub Actions!'); --- IGNORE ---
 });
 });
